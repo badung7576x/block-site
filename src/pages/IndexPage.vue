b2583239be258df6d7bb1627df7b4c4134f61656
badung7576 <dung.dang@pirago.work>
2022-10-11 20:01:37 +0700

    init project

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,23 @@
+<template>
+  <q-page class="row items-center justify-evenly">
+    <!-- <example-component
+      title="Example component"
+      active
+      :todos="todos"
+      :meta="meta"
+    ></example-component> -->
+  </q-page>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+
+export default defineComponent({
+  name: 'IndexPage',
+  components: {},
+  setup() {
+
+    return {};
+  }
+});
+</script>
